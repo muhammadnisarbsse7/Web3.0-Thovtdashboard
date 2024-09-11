8042ad78755b25652e46c5d43a45d8027f8345c7
Muhammad Nisar <124270450+muhammadnisarbsse7@users.noreply.github.com>
2024-09-11 22:32:01 +0500

    Treasury updated Code

---
+++ src/Svg/TreasuryAssetSvg/ExclamatoryIcon.tsx
@@ -0,0 +1,15 @@
+export const ExclamatoryIcon = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="6" cy="6" r="6" fill="#808A9D" />
+      <line x1="6.5" y1="5" x2="6.5" y2="8.6" stroke="white" />
+      <circle cx="6.5" cy="3.5" r="0.5" fill="white" />
+    </svg>
+  );
+};
--- src/pages/TreasuryAsset.tsx
+++ src/pages/TreasuryAsset.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import { LocationIcon } from '../Svg/TreasuryAssetSvg/LocationIcon';
 import { KeyIcon } from '../Svg/TreasuryAssetSvg/KeyIcon';
 import { ContactIcon } from '../Svg/TreasuryAssetSvg/ContactIcon';
@@ -7,12 +7,19 @@ import HouseTwo from '../images/TreasuryAssetImg/House2.png';
 import HouseThree from '../images/TreasuryAssetImg/House3.png';
 import HouseFour from '../images/TreasuryAssetImg/House4.png';
 import HouseFive from '../images/TreasuryAssetImg/House5.png';
+import { ExclamatoryIcon } from '../Svg/TreasuryAssetSvg/ExclamatoryIcon';
 
 const TreasuryAsset = () => {
+  const [activeButton, setActiveButton] = useState('Partners');
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
   return (
     <div className="grid-row flex flex-col  sm:grid sm:grid-cols-12 sm:gap-3">
       <div className="grid sm:col-span-8 bg-[#151517] h-full rounded-3xl">
-        <div className="flex flex-col border mt-20 pl-4 md:pl-8 lg:pl-12">
+        <div className="flex flex-col mt-20 pl-4 md:pl-8 lg:pl-12">
           <h1 className="font-inter font-semibold text-white text-lg lg:text-2xl leading-5">
             Karra Loft 3 A
           </h1>
@@ -38,10 +45,10 @@ const TreasuryAsset = () => {
             </div>
           </div>
           <div className="grid sm:grid-cols-12 gap-[18px] ">
-            <div className="sm:col-span-8 sm:mt-5 border h-full">
+            <div className="sm:col-span-8 sm:mt-5  h-full">
               <img src={HouseOne} alt="One" />
             </div>
-            <div className="sm:col-span-3 sm:mt-5 flex flex-col border space-y-2">
+            <div className="sm:col-span-3 sm:mt-5 flex flex-col  space-y-2">
               <img src={HouseTwo} alt="Two" />
               <img src={HouseThree} alt="Three" />
               <img src={HouseFour} alt="Four" />
@@ -51,12 +58,137 @@ const TreasuryAsset = () => {
         </div>
       </div>
       <div className="grid sm:col-span-4 bg-[#151517] h-[780px] rounded-3xl">
-        <div className="flex flex-row">
-          <div className=" border-2">
-            <div>Property Price</div>
-            <div>
-              $220,000 <span>USDT</span>
+        <div className="flex flex-col h-20 w-full mt-16">
+          <div className=" flex  items-center gap-8 justify-between mx-auto">
+            <div className="flex flex-col space-y-3 ">
+              <div className="font-inter font-semibold text-white text-[20px] leading-5 ">
+                Property Price
+              </div>
+              <div className="font-inter font-semibold text-[#FFE9A4] text-[20px] leading-5 ">
+                $220,000{' '}
+                <span className="font-inter font-semibold text-[14px] text-[#7D8090] leading-[14px]">
+                  USDT
+                </span>
+              </div>
             </div>
+            <div className="h-[93px] w-[2px] bg-[#7D8090]"></div>
+            <div className="flex flex-col space-y-2">
+              <div className="flex justify-center items-center gap-2">
+                <div className="font-inter font-normal text-white text-[14px] leading-[14px] ">
+                  IRR
+                </div>
+                <div>
+                  <ExclamatoryIcon />
+                </div>
+                <div className="font-inter font-semibold text-[#FFE9A4] text-[14px] leading-[14px] ">
+                  31.3%
+                </div>
+              </div>
+              <div className="flex justify-center items-center gap-2">
+                <div className="font-inter font-normal text-white text-[14px] leading-[14px] ">
+                  IRR
+                </div>
+                <div>
+                  <ExclamatoryIcon />
+                </div>
+                <div className="font-inter font-semibold text-[#FFE9A4] text-[14px] leading-[14px] ">
+                  31.3%
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col mx-auto  ">
+            <div className="flex justify-between gap-28  mt-8 ">
+              <div className="font-inter font-semibold text-[10px] text-white leading-[10px] ">
+                Token Price:
+                <span className="font-inter font-semibold text-[#FFE9A4] text-[14px] leading-[14px]">
+                  $50
+                </span>
+              </div>
+              <div className="font-inter font-semibold text-[10px] text-white leading-[10px]">
+                Collection: 100.00 %
+              </div>
+            </div>
+            <div className="mt-3 h-2  w-full bg-[#FFE9A4] rounded-[4px]"></div>
+            <div className="w-full border border-[#7D8090] mt-8"></div>
+
+            <div className="h-[190px] w-full rounded-[24px] bg-[#FFE9A4] mt-7">
+              <div className="flex flex-col justify-center items-center mt-5">
+                <div className="flex flex-col justify-center items-center">
+                  <div className="h-[6px] w-[1px] bg-black"></div>
+                  <div className="h-[31px] w-[48px] bg-[#6E6748] border border-black flex justify-center items-center">
+                    <h1 className="font-inter font-normal text-white text-[10px] leading-[10px] ">
+                      Sold out
+                    </h1>
+                  </div>
+                  <div className="h-[6px] w-[1px] bg-black"></div>
+                </div>
+                <div className="font-inter font-semibold text-black text-[20px] leading-5  mt-2">
+                  Property is sold out!
+                </div>
+                <div className="h-[40px] w-[180px] rounded-[10px] bg-gradient-to-t from-[#262626] to-[#363636] mt-[18px] flex justify-center items-center">
+                  <h1 className="font-inter font-semibold text-[#FFE9A4] text-[14px] leading-[14px]  ">
+                    Secondary Market
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="flex rounded-[24px]  bg-[#FFE9A4]   gap-[1px] mt-[27px]">
+              <button
+                onClick={() => handleButtonClick('All')}
+                className={`px-6 rounded-[17px] ${
+                  activeButton === 'All'
+                    ? 'bg-[#FBF4F4] text-black border border-black py-1'
+                    : 'text-black'
+                }`}
+              >
+                Partners
+              </button>
+              <button
+                onClick={() => handleButtonClick('Active')}
+                className={`px-5 rounded-[17px] ${
+                  activeButton === 'Active'
+                    ? 'bg-[#FBF4F4] text-black border border-black py-1'
+                    : 'text-black'
+                }`}
+              >
+                Owners
+              </button>
+              <button
+                onClick={() => handleButtonClick('Archive')}
+                className={`px-4 rounded-[17px] ${
+                  activeButton === 'Archive'
+                    ? 'bg-[#FBF4F4] text-black border border-black py-1'
+                    : 'text-black'
+                }`}
+              >
+                Activities
+              </button>
+            </div>
+            <div className="flex mt-8 gap-2">
+              <div className="h-12 w-12 bg-[#D9D9D9] rounded-[10px]"></div>
+              <div className="flex flex-col  justify-between">
+                <div className="font-inter font-semibold text-[#FFFDFD] text-[14px] leading-[14px] ">
+                  Bali Business Consulting{' '}
+                </div>
+                <div className="font-inter font-semibold text-[#7D8090]  text-[14px] leading-[14px]">
+                  Legal
+                </div>
+              </div>
+            </div>
+            <div className="w-full border-b border-dashed border-[#7D8090] mt-5"></div>
+            <div className="flex mt-8 gap-2">
+              <div className="h-12 w-12 bg-[#D9D9D9] rounded-[10px]"></div>
+              <div className="flex flex-col  justify-between">
+                <div className="font-inter font-semibold text-[#FFFDFD] text-[14px] leading-[14px] ">
+                  Bali Invest Management
+                </div>
+                <div className="font-inter font-semibold text-[#7D8090]  text-[14px] leading-[14px]">
+                  Management
+                </div>
+              </div>
+            </div>
+            <div className="w-full border-b border-dashed border-[#7D8090] mt-5"></div>
           </div>
         </div>
       </div>
