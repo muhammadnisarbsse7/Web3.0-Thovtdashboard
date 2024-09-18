0ad87ada701fdcaffeea885e46e6df0fd4ac613b
Muhammad Nisar <124270450+muhammadnisarbsse7@users.noreply.github.com>
2024-09-18 20:52:48 +0500

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -125,7 +125,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     <React.Fragment>
                       <NavLink
                         to="/"
-                        className="group   relative flex items-center  gap-2.5 rounded-md px-4 font-normal text-white duration-300 ease-in-out hover:text-white"
+                        className="group relative flex items-center  gap-2.5 rounded-md px-4 font-normal text-white duration-300 ease-in-out hover:text-white"
                       >
                         {({ isActive }) => (
                           <>
