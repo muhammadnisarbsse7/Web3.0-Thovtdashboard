7688de6473ade032db6b72dfff07b8b4a26fb7c7
Muhammad Nisar <124270450+muhammadnisarbsse7@users.noreply.github.com>
2024-09-19 19:51:55 +0500

    header shadow issue fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,7 +16,7 @@ const Header = (props: {
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-[#090909] drop-shadow-1 ">
+    <header className="sticky top-0 z-999 flex w-full bg-[#090909]  ">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11 ">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
